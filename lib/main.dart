bc32d6681d5d30b3097bc352ad69a31005c831de
Barat Narcis-Stefan (uie46448) <narcis-stefan.barat@continental-corporation.com>
2022-04-22 16:37:17 +0300

    Almost finished, still a problem with lg

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:math_expressions/math_expressions.dart';
 
@@ -33,7 +35,7 @@ class _SimpleCalculatorState extends State<SimpleCalculator> {
 
   buttonPressed(String buttonText) {
     setState(() {
-      if (buttonText == "AC") {
+      if (buttonText == "C") {
         equation = "0";
         result = "0";
         equationFontSize = 38.0;
@@ -52,6 +54,10 @@ class _SimpleCalculatorState extends State<SimpleCalculator> {
         expression = equation;
         expression = expression.replaceAll('×', '*');
         expression = expression.replaceAll('÷', '/');
+        expression = expression.replaceAll('√', 'sqrt');
+        expression = expression.replaceAll('e', '2.71828182846');
+        expression = expression.replaceAll('π', '3.14159265359');
+        expression = expression.replaceAll('lg', 'log');
 
         try {
           Parser p = Parser();
@@ -59,12 +65,38 @@ class _SimpleCalculatorState extends State<SimpleCalculator> {
 
           ContextModel cm = ContextModel();
           result = '${exp.evaluate(EvaluationType.REAL, cm)}';
+
+          double dblRes = double.parse(result);
+          var x = dblRes - dblRes.truncate();
+
+          if (x == 0.0){
+              result = result.replaceAll('.0', '');
+          }
+
+          if(result.length > 20) {
+            resultFontSize = 25.0;
+            equationFontSize = 20.0;
+          } else if(result.length > 10) {
+            resultFontSize = 38.0;
+            equationFontSize = 28.0;
+          } else {
+            resultFontSize = 48.0;
+            equationFontSize = 38.0;
+          }
         } catch (e) {
           result = "Error";
         }
       } else {
-        equationFontSize = 48.0;
-        resultFontSize = 38.0;
+        if (equation.length > 16) {
+          equationFontSize = 25.0;
+          resultFontSize = 20.0;
+        } else if (equation.length > 10){
+          equationFontSize = 38.0;
+          resultFontSize = 28.0;
+        } else {
+          equationFontSize = 48.0;
+          resultFontSize = 38.0;
+        }
         if (equation == "0") {
           equation = buttonText;
         } else {
@@ -74,16 +106,36 @@ class _SimpleCalculatorState extends State<SimpleCalculator> {
     });
   }
 
-  Widget buildButton(
-      String buttonText, double buttonHeight, Color buttonColor, Color textColor) {
+  Widget buildButton(String buttonText, double buttonHeight, Color buttonColor,
+      Color textColor, double fontSz) {
     return Container(
       height: MediaQuery.of(context).size.height * 0.1 * buttonHeight,
-      width: MediaQuery.of(context).size.width * 0.5,
       color: buttonColor,
       child: FlatButton(
           shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-              ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          padding: EdgeInsets.all(16.0),
+          onPressed: () => buttonPressed(buttonText),
+          child: Text(
+            buttonText,
+            style: TextStyle(
+                fontSize: fontSz,
+                fontWeight: FontWeight.normal,
+                color: textColor),
+          )),
+    );
+  }
+
+  Widget buildSecondaryButton(String buttonText, double buttonHeight,
+      Color buttonColor, Color textColor) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.1 * buttonHeight,
+      color: buttonColor,
+      child: FlatButton(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
           padding: EdgeInsets.all(16.0),
           onPressed: () => buttonPressed(buttonText),
           child: Text(
@@ -99,7 +151,11 @@ class _SimpleCalculatorState extends State<SimpleCalculator> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Doofenshmirtz Scientific Narcilator', textAlign: TextAlign.right,)),
+      appBar: AppBar(
+          title: Text(
+        'Doofenshmirtz Scientific Narcilator',
+        textAlign: TextAlign.right,
+      )),
       body: Column(
         children: <Widget>[
           Container(
@@ -112,7 +168,7 @@ class _SimpleCalculatorState extends State<SimpleCalculator> {
           ),
           Container(
             alignment: Alignment.centerRight,
-            padding: EdgeInsets.fromLTRB(10, 30, 10, 0),
+            padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
             child: Text(
               result,
               style: TextStyle(fontSize: resultFontSize, color: Colors.white),
@@ -129,34 +185,47 @@ class _SimpleCalculatorState extends State<SimpleCalculator> {
                 child: Table(
                   children: [
                     TableRow(children: [
-                      buildButton("AC", 1, Colors.black54, Colors.orange),
-                      buildButton("⌫", 1, Colors.black54, Colors.orange),
-                      buildButton("%", 1, Colors.black54, Colors.orange),
-                      buildButton("÷", 1, Colors.black54, Colors.orange),
+                      buildButton(" π", 1, Colors.black54, Colors.grey, 20.0),
+                      buildButton("lg", 1, Colors.black54, Colors.grey, 20.0),
+                      buildButton("ln", 1, Colors.black54, Colors.grey, 20.0),
+                      buildButton("(", 1, Colors.black54, Colors.grey, 30.0),
+                      buildButton(")", 1, Colors.black54, Colors.grey, 30.0),
                     ]),
                     TableRow(children: [
-                      buildButton("7", 1, Colors.black54, Colors.white),
-                      buildButton("8", 1, Colors.black54, Colors.white),
-                      buildButton("9", 1, Colors.black54, Colors.white),
-                      buildButton("×", 1, Colors.black54, Colors.orange),
+                      buildButton("sin", 1, Colors.black54, Colors.grey, 20.0),
+                      buildButton("C", 1, Colors.black54, Colors.orange, 30.0),
+                      buildButton("⌫", 1, Colors.black54, Colors.orange, 30.0),
+                      buildButton("%", 1, Colors.black54, Colors.orange, 30.0),
+                      buildButton("÷", 1, Colors.black54, Colors.orange, 30.0),
                     ]),
                     TableRow(children: [
-                      buildButton("4", 1, Colors.black54, Colors.white),
-                      buildButton("5", 1, Colors.black54, Colors.white),
-                      buildButton("6", 1, Colors.black54, Colors.white),
-                      buildButton("-", 1, Colors.black54, Colors.orange),
+                      buildButton("cos", 1, Colors.black54, Colors.grey, 20.0),
+                      buildButton("7", 1, Colors.black54, Colors.white, 30.0),
+                      buildButton("8", 1, Colors.black54, Colors.white, 30.0),
+                      buildButton("9", 1, Colors.black54, Colors.white, 30.0),
+                      buildButton("×", 1, Colors.black54, Colors.orange, 30.0),
                     ]),
                     TableRow(children: [
-                      buildButton("1", 1, Colors.black54, Colors.white),
-                      buildButton("2", 1, Colors.black54, Colors.white),
-                      buildButton("3", 1, Colors.black54, Colors.white),
-                      buildButton("+", 1, Colors.black54, Colors.orange),
+                      buildButton("tan", 1, Colors.black54, Colors.grey, 20.0),
+                      buildButton("4", 1, Colors.black54, Colors.white, 30.0),
+                      buildButton("5", 1, Colors.black54, Colors.white, 30.0),
+                      buildButton("6", 1, Colors.black54, Colors.white, 30.0),
+                      buildButton("-", 1, Colors.black54, Colors.orange, 30.0),
                     ]),
                     TableRow(children: [
-                      buildButton("R", 1, Colors.black54, Colors.orange),
-                      buildButton("0", 1, Colors.black54, Colors.white),
-                      buildButton(".", 1, Colors.black54, Colors.white),
-                      buildButton("=", 1, Colors.deepPurple, Colors.white),
+                      buildButton("√", 1, Colors.black54, Colors.grey, 20.0),
+                      buildButton("1", 1, Colors.black54, Colors.white, 30.0),
+                      buildButton("2", 1, Colors.black54, Colors.white, 30.0),
+                      buildButton("3", 1, Colors.black54, Colors.white, 30.0),
+                      buildButton("+", 1, Colors.black54, Colors.orange, 30.0),
+                    ]),
+                    TableRow(children: [
+                      buildButton(" e", 1, Colors.black54, Colors.white, 30.0),
+                      buildButton("0", 1, Colors.black54, Colors.white, 30.0),
+                      buildButton("00", 1, Colors.black54, Colors.white, 30.0),
+                      buildButton(".", 1, Colors.black54, Colors.white, 30.0),
+                      buildButton(
+                          "=", 1, Colors.deepPurple, Colors.white, 30.0),
                     ]),
                   ],
                 ),
